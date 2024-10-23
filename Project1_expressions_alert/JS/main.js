6e50047da4f9e36921b6624d0e4e7540bd41b67b
Psykiht <ironyhumble@gmail.com>
2024-10-23 09:17:04 +0100

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+//Window.alert method
+window.alert("Hello today!");
+
+//write method
+document.write("Hello again, incase you did not read the alert");
+
+//variables
+var Sent1 = "This is Start of string";
+var Sent2 = "and this is End";
+
+//display variables
+document.write(Sent1 + Sent2);
+
+//expression
+document.write(21 * 42);
